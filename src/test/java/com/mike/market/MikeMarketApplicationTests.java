21677893e369d2feccc25a51f783b76a4376ec43
Miguel Mejia <miguelmejia92@outlook.com>
2020-11-04 22:29:10 -0600

    Creacion de controller para prueba y configuracion de puerto y perfiles con .properties

---
+++ src/test/java/com/mike/market/MikeMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mike.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MikeMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
