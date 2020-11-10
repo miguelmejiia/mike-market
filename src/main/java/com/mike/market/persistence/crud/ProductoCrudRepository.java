84ad72a8ddabaabfddac2f324d4ff4791cc4fecc
Miguel Mejia <miguelmejia92@outlook.com>
2020-11-09 19:22:12 -0600

    Creacion de queryMethods y uso de map structs

---
+++ src/main/java/com/mike/market/persistence/crud/ProductoCrudRepository.java
@@ -0,0 +1,13 @@
+package com.mike.market.persistence.crud;
+
+import com.mike.market.persistence.entity.Producto;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
+}
