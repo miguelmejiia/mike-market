9f90e7bf9de946cfa08126e5eb4a231afd980e5c
Miguel Mejia <miguelmejia92@outlook.com>
2020-11-10 19:45:38 -0600

    Inyeccion de dependencias

--- src/main/java/com/mike/market/persistence/ProductoRepository.java
+++ src/main/java/com/mike/market/persistence/ProductoRepository.java
@@ -5,6 +5,7 @@ import com.mike.market.domain.repository.ProductRepository;
 import com.mike.market.persistence.crud.ProductoCrudRepository;
 import com.mike.market.persistence.entity.Producto;
 import com.mike.market.persistence.mapper.ProductMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,8 +13,10 @@ import java.util.Optional;
 
 @Repository
 public class ProductoRepository implements ProductRepository {
+    @Autowired
     private ProductoCrudRepository productoCrudRepository;
 
+    @Autowired
     private ProductMapper mapper;
 
     @Override
