e46a748626ec06a1b98c0944c634a8a51fdee9d0
Miguel Mejia <miguelmejia92@outlook.com>
2020-12-06 00:20:28 -0600

    Arreglo de bug en la validacion de usuario

--- src/main/java/com/mike/market/domain/service/MikeUserDetailsService.java
+++ src/main/java/com/mike/market/domain/service/MikeUserDetailsService.java
@@ -6,13 +6,22 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MikeUserDetailsService implements UserDetailsService {
 
+    private static List<User> users = new ArrayList<>();
+
+    public MikeUserDetailsService(){
+        users.add(new User("miguel", "{noop}mik3", new ArrayList<>()));
+    }
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = users.stream().filter(u -> u.getUsername().equals(username)).findAny();
         return new User("miguel", "{noop}mik3", new ArrayList<>());
     }
 }
